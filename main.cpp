e95bf674344759908c21886c7ef552cc2aabebc8
DarkOut0 <56633977+DarkOut0@users.noreply.github.com>
2020-09-21 16:15:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop Go!!!!!!!!!!!!!!!!!!!To*/
-
+ 
 int main(int argc, char** argv) {
 	int Array[5] = {1, 2, 3, 4, 5};
 	int choise;
